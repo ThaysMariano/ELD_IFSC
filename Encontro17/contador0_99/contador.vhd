fbb73ca76900cd80f2d968dc999e38145242f30a
thays <thaysdasilvamariano@gmail.com>
2024-11-14 15:33:43 -0300

    Atividade contador de 0 a 99. [Erro -> 9 para 16]

--- Encontro17/contador0_99/contador.vhd
+++ Encontro17/contador0_99/contador.vhd
@@ -1,36 +1,64 @@
 library ieee;
  use ieee.std_logic_1164.all;
+ use ieee.numeric_std.all;
  
 entity contador is
-	generic (N : natural := 8);
 	port(
-        clk,reset: in std_logic;
-        dezena, unidade: buffer integer 0 to 9;
-		  numero : out integer 0 to 99
-        );
-end entity;
+		clk, reset: in std_logic;
+		numero     : out std_logic_vector(7 downto 0)
+	);
+end contador;
 
 architecture ifsc_v1 of contador is
+	signal r_regUN: unsigned(3 downto 0); 
+	signal r_regDZ: unsigned(3 downto 0); 
+	signal r_nextUN: unsigned(3 downto 0);
+	signal r_nextDZ: unsigned(3 downto 0);
+ begin
+ 
+ -- register
+ process(clk,reset)
+	begin
+	if (reset='1') then
+		r_regUN <= (others => '0');
+		r_regDZ <= (others => '0');	
+	elsif (clk'event and clk='1') then
+		r_regUN <= r_nextUN;
+		r_regDZ <= r_nextDZ;
+	end if;
+end process;
 
-	signal count1 : integer 0 to 9;
-	signal count2 : integer 0 to 9;
-	
-begin
-	count1 := 0;
-	coutn2 := 0;
-
-process(clk, reset)
-
+ -- next-state logic
+			process(r_regUN, r_regDZ)
     begin
-        if (reset='1') then
-            unidade <= 0;
-				dezena <= 0;
-        elsif (clk'event and clk='1') then
-            unidade <= count1 + 1
-				wait until unidade = 9;
-					dezena = count2 +1;
+        if r_regUN = 9 then
+            r_nextUN <= (others => '0'); 
+            r_nextDZ <= r_regDZ + 1;     
+        elsif r_regDZ = 9 then 
+				r_nextDZ <= (others => '0'); 
+			else
+            r_nextUN <= r_regUN + 1; 
+            r_nextDZ <= r_regDZ;      
         end if;
     end process;
-	 numero <= dezena + unidade;
+ 
+ -- output logic
+   numero <= std_logic_vector(r_regDZ) & std_logic_vector(r_regUN);
 
-end architecture;
+end ifsc_v1;
+
+
+
+--unidade <= 0;
+--	 dezena <= 0;
+--	 
+--	 auxu <= 0;
+--	 auxd <=0;
+--	 
+--	 unidade <= auxu +1;
+--			if unidade = 9 then
+--				dezena <= auxd +1;
+--				unidade <= 0;
+--			end if;
+--
+--	 numero <= dezena & unidade;
