e40fdf0d7c5ef6754d751520d5963bec06ca4b70
thays <thaysdasilvamariano@gmail.com>
2024-11-14 11:36:57 -0300

    tentativa contar primeiros zeros

--- Avaliacoes/contadorZerosGenerico/Zerostotais/leading_zeros_generate.vhd
+++ Avaliacoes/contadorZerosGenerico/Zerostotais/leading_zeros_generate.vhd
@@ -32,14 +32,29 @@ end entity;
 
 architecture ifsc_v1 of leading_zeros_generate is
 	signal tmp : integer_vector (0 to N);
-begin
-
-	tmp(0) <= 0;
-	l1:for i in 1 to N generate
-			tmp(i) <= tmp(i-1) + 1 when data(i-1) = 0 else
-						tmp(i-1);
-	end generate;
+	signal aux : integer_vector  (0 to N);
 	
-				resultado <= tmp(N);
+	begin
+
+	--							0 0 0 1 0 0 1 0     -> 3
+	tmp(0) <= 0;
+	aux<=0;
+	l1: for i in 1 to N generate 
+		tmp(i) <= i when data(i-1) = 1 else
+						0;
+			end generate;
+			
+			l2: for i in 1 to N generate
+				aux(N) <= i when data(i-1) =1 else
+						0;
+					end generate;
+			
+			-- 0 0 0 4 0 0 7 0
+			
+	
+--	N-data(i)-1 <- data i sendo o primeiro 1
+	
+	
+				resultado <= tmp(N)-aux(N);
 
 end architecture;
